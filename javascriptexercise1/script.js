57eae5d440abe4c34ad538d3ce46dae2d2e1bbc1
pasanen13 <ppasanen1983@aol.com>
2016-07-25 21:37:51 -0400

    new challenge

--- javascriptexercise1/script.js
+++ javascriptexercise1/script.js
@@ -11,6 +11,12 @@ console.log(myButtons);
 myButtons[0].addEventListener("click", function () {
 	//alert("you click me! yay! haha!");
     sandwichImagesElems[0].src = "http://img.foodnetwork.com/FOOD/2012/01/06/HE_sports-drinks_s4x3_lead.jpg";
+});
+
+myButtons[1].addEventListener("click", function	() {
+	sandwichImagesElems[0].className = "red-border";
+	sandwichImagesElems[0].className = "red-border";
+    
 
 });
 
